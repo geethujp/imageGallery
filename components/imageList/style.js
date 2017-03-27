09ebdd16da81d3f1fb208b9fe31b110da36ac27b
geethuj <geethuj@qburst.com>
2017-03-27 14:21:59 +0530

    Used Dimensions object to determine the image width

--- components/imageList/style.js
+++ components/imageList/style.js
@@ -1,36 +1,23 @@
 /**
  * Gallery Style sheet
  */
- import { StyleSheet } from 'react-native';
+import { StyleSheet, Dimensions } from 'react-native';
 
 export default StyleSheet.create({
-  image: {
-    height: 100,
-    width: 100
-  },
-  title: {
-    fontSize: 20,
-    textAlign: 'center',
-    margin: 10,
-    color: '#5c6bc0',
-  },
-  container: {
-        flex:1,
-        flexDirection:'row',
-        alignItems: 'center',
+    image: {
+        height: 110,
+        width: Dimensions.get('window').width / 2 ,
+        resizeMode: 'cover'//Device width divided in almost a half
     },
-    info_wrapper: {
-        fontSize: 16,
+    headline: {
+        color: '#000',
         textAlign: 'center',
-        margin: 15,
-        padding: 10,
-        borderRadius: 5,
-        borderWidth: 0.5,
-        borderColor: '#5c6bc0',
-        color: '#5c6bc0',
     },
-    proceed: {
-        flex:1,
-        margin:20,
-    }
+    list_view: {
+        flexDirection: 'row',
+        justifyContent: 'space-around',
+        alignItems: 'center',
+        flexWrap: 'wrap',
+    },
+
 });
