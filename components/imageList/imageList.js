acf36322c7630241dc6b8ae37942184d571fc1e7
geethuj <geethuj@qburst.com>
2017-03-30 16:19:22 +0530

    Assigned to width and height to variable

--- components/imageList/imageList.js
+++ components/imageList/imageList.js
@@ -1,6 +1,6 @@
 /**
  * image List Component
- * List all the images s
+ * List all the imagess
  */
 import React, { Component } from 'react';
 import {
@@ -13,8 +13,9 @@ import {
 
 } from 'react-native';
 import styles from './style';
-
-
+import Device from '../../lib/device-info';
+const deviceWidth= Dimensions.get('window').width;
+const deviceHeight= Dimensions.get('window').height;
 export default class imageList extends Component {
     onPress = () => {
         const { navigator } = this.props;
@@ -27,14 +28,14 @@ export default class imageList extends Component {
         var ds = new ListView.DataSource({ rowHasChanged: (r1, r2) => r1 !== r2 });
         this.state = {
             dataSource: ds.cloneWithRows([
-                { 'name': 'Image1', 'image': 'http://i.imgur.com/xIzskJl.jpg' },
-                { 'name': 'Image2', 'image': 'http://i.imgur.com/xIzskJl.jpg' },
-                { 'name': 'Image3', 'image': 'http://i.imgur.com/xIzskJl.jpg' },
-                { 'name': 'Image4', 'image': 'http://i.imgur.com/xIzskJl.jpg' },
-                { 'name': 'Image5', 'image': 'http://i.imgur.com/xIzskJl.jpg' },
-                { 'name': 'Image6', 'image': 'http://i.imgur.com/xIzskJl.jpg' },
-                { 'name': 'Image7', 'image': 'http://i.imgur.com/xIzskJl.jpg' },
-                { 'name': 'Image8', 'image': 'http://i.imgur.com/xIzskJl.jpg' },
+                { 'name': 'Image1', 'image': '../../images/pic_01.jpg' },
+                { 'name': 'Image2', 'image': '../../images/pic_02.jpg' },
+                { 'name': 'Image3', 'image': '../../images/pic_03.jpg' },
+                { 'name': 'Image4', 'image': '../../images/pic_04.jpg' },
+                { 'name': 'Image5', 'image': '../../images/pic_05.jpg' },
+                { 'name': 'Image6', 'image': '../../images/pic_06.jpg' },
+                { 'name': 'Image7', 'image': '../../images/pic_07.jpg' },
+                { 'name': 'Image8', 'image': '../../images/pic_08.jpg' },
             ]),
         }
     }
@@ -47,24 +48,6 @@ export default class imageList extends Component {
         );
     }
     render() {
-        const Device = require('react-native-device-detection'),
-              deviceWidth= Dimensions.get('window').width,
-              deviceHeight= Dimensions.get('window').height;
-        if (Device.pixelDensity < 2 && (Device.adjustedWidth >= 1000 || Device.adjustedHeight >= 1000)) {
-            Device.isTablet = true;
-            Device.isPhone = false;
-        } else if (Device.pixelDensity === 2 && (Device.adjustedWidth >= 1920 || Device.adjustedHeight >= 1920)) {
-            Device.isTablet = true;
-            Device.isPhone = false;
-        }
-         else if (Device.pixelDensity === 1 && (Device.adjustedWidth == 600 || Device.adjustedHeight == 960)) {
-            Device.isTablet = true;
-            Device.isPhone = false;
-        } else {
-            Device.isTablet = false;
-            Device.isPhone = true;
-        }
-
         if (Device.isTablet) {
             Object.assign(styles, {
                 image: {
