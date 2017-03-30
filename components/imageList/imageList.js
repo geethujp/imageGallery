ab6325749b5fbc918958aaa0da366cde0302bff0
geethuj <geethuj@qburst.com>
2017-03-30 12:53:24 +0530

    Fixed the Nexus 7 mobile behaviour

--- components/imageList/imageList.js
+++ components/imageList/imageList.js
@@ -10,85 +10,82 @@ import {
     ListView,
     Button,
     Dimensions,
-    PixelRatio,
 
 } from 'react-native';
 import styles from './style';
 
 
 export default class imageList extends Component {
-  onPress = () => {
-    const { navigator } = this.props;
-    navigator.push({
-      id: 'Info'
-    });
-  }
-  constructor(){
-    this.pixelDensity = PixelRatio.get();
-    this.width = windowSize.width;
-    this.height = windowSize.height;
-    this.adjustedWidth = this.width * this.pixelDensity;
-    this.adjustedHeight = this.height * this.pixelDensity;
+    onPress = () => {
+        const { navigator } = this.props;
+        navigator.push({
+            id: 'Info'
+        });
+    }
+    constructor() {
+        super();
+        var ds = new ListView.DataSource({ rowHasChanged: (r1, r2) => r1 !== r2 });
+        this.state = {
+            dataSource: ds.cloneWithRows([
+                { 'name': 'Image1', 'image': 'http://i.imgur.com/xIzskJl.jpg' },
+                { 'name': 'Image2', 'image': 'http://i.imgur.com/xIzskJl.jpg' },
+                { 'name': 'Image3', 'image': 'http://i.imgur.com/xIzskJl.jpg' },
+                { 'name': 'Image4', 'image': 'http://i.imgur.com/xIzskJl.jpg' },
+                { 'name': 'Image5', 'image': 'http://i.imgur.com/xIzskJl.jpg' },
+                { 'name': 'Image6', 'image': 'http://i.imgur.com/xIzskJl.jpg' },
+                { 'name': 'Image7', 'image': 'http://i.imgur.com/xIzskJl.jpg' },
+                { 'name': 'Image8', 'image': 'http://i.imgur.com/xIzskJl.jpg' },
+            ]),
+        }
+    }
 
-    this.isPhoneOrTablet();
-    isPhoneOrTablet() {
-    if(this.pixelDensity < 2 && (this.adjustedWidth >= 1000 || this.adjustedHeight >= 1000)) {
-      this.isTablet = true;
-      this.isPhone = false;
-    } else if(this.pixelDensity === 2 && (this.adjustedWidth >= 1920 || this.adjustedHeight >= 1920)) {
-      this.isTablet = true;
-      this.isPhone = false;
-    } else {
-      this.isTablet = false;
-      this.isPhone = true;
+    renderCategory(category) {
+        return (
+          <Image source = {{uri: category.image}} style = {styles.image}>
+            <Text style = {styles.headline}>{category.name}</Text>
+          </Image>
+        );
     }
-  }
-    super();
-    var ds = new ListView.DataSource({rowHasChanged: (r1, r2) => r1 !== r2});
-    this.state = {
-      dataSource: ds.cloneWithRows([
-        {'name':'Image1', 'image':'http://i.imgur.com/xIzskJl.jpg'},
-        {'name':'Image2', 'image':'http://i.imgur.com/xIzskJl.jpg'},
-        {'name':'Image3', 'image':'http://i.imgur.com/xIzskJl.jpg'},
-        {'name':'Image4', 'image':'http://i.imgur.com/xIzskJl.jpg'},
-        {'name':'Image5', 'image':'http://i.imgur.com/xIzskJl.jpg'},
-        {'name':'Image6', 'image':'http://i.imgur.com/xIzskJl.jpg'},
-        {'name':'Image7', 'image':'http://i.imgur.com/xIzskJl.jpg'},
-        {'name':'Image8', 'image':'http://i.imgur.com/xIzskJl.jpg'},
-      ]),
-    }
-  }
+    render() {
+        const Device = require('react-native-device-detection'),
+              deviceWidth= Dimensions.get('window').width,
+              deviceHeight= Dimensions.get('window').height;
+        if (Device.pixelDensity < 2 && (Device.adjustedWidth >= 1000 || Device.adjustedHeight >= 1000)) {
+            Device.isTablet = true;
+            Device.isPhone = false;
+        } else if (Device.pixelDensity === 2 && (Device.adjustedWidth >= 1920 || Device.adjustedHeight >= 1920)) {
+            Device.isTablet = true;
+            Device.isPhone = false;
+        }
+         else if (Device.pixelDensity === 1 && (Device.adjustedWidth == 600 || Device.adjustedHeight == 960)) {
+            Device.isTablet = true;
+            Device.isPhone = false;
+        } else {
+            Device.isTablet = false;
+            Device.isPhone = true;
+        }
 
-  renderCategory(category) {
-    return (
-        <Image source={{uri: category.image}} style={styles.image}>
-            <Text style={styles.headline}>{category.name}</Text>
-        </Image>
-      );
+        if (Device.isTablet) {
+            Object.assign(styles, {
+                image: {
+                    width: deviceWidth / 5 - 10,
+                    height: deviceWidth/ 5 - 10,
+                }
+            });
+        }
+        if (Device.isPhone) {
+            Object.assign(styles, {
+                image: {
+                    width: deviceWidth / 3 - 10,
+                    height: deviceWidth / 3 - 10,
+                }
+            });
+        }
+        return (
+            <View>
+              <Button title = "Back" color = "#5c6bc0" onPress = { this.onPress }/>
+              <ListView dataSource = {this.state.dataSource} renderRow = {this.renderCategory} contentContainerStyle = {styles.list_view}/>
+            </View>
+        );
     }
-  render() {
-     const Device = require('react-native-device-detection');
-  if(Device.isTablet) {
-  Object.assign(styles, {
-   image: {
-     width: Dimensions.get('window').width / 5 - 10 ,
-     height: Dimensions.get('window').width / 5 - 10,
-   }
-  });
-}
-if(Device.isPhone) {
-  Object.assign(styles, {
-   image: {
-     width: Dimensions.get('window').width / 3 - 10 ,
-     height: Dimensions.get('window').width / 3 - 10,
-   }
-  });
-}
-    return (
-      <View>
-      <Button title="Back" color="#5c6bc0" onPress={this.onPress}/>
-      <ListView dataSource={this.state.dataSource} renderRow={this.renderCategory} contentContainerStyle={styles.list_view}/>
-      </View>
-    );
-  }
 }
